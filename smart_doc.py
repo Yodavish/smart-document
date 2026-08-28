0ee519b2f120a2f512a21ec660e1be5286e0188d
David Tran <tranvu52@gmail.com>
2026-08-28 10:48:26 -0500

    Refactor document processing with OOP

--- smart_doc.py
+++ smart_doc.py
@@ -7,37 +7,53 @@ pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 )
 
+class DocumentProcessor:
+    def __init__(self, filename):
+        self.filename = filename
+        self.document = None
+        self.page_text = []
+        self.chunks = []
+
+    def open_documents(self):
+        self.document = pymupdf.open(self.filename)
+        
+    def extract_pages(self):
+        extracted_text = []
+
+        for page in self.document:
+            text = page.get_text()
+
+            if not text.strip():
+                pix = page.get_pixmap()
+                image = Image.open(
+                    BytesIO(pix.tobytes("png"))
+                )
+
+                text = self.perform_ocr(image)
+
+            extracted_text.append(text)
+
+        self.page_text = extracted_text
+
+    def perform_ocr(self, image):
+        return pytesseract.image_to_string(image)
+
+    def chunk_page(self, chunk_page_size = 5):
+        for i in range(0, len(self.page_text), chunk_page_size):
+            self.chunks.append(self.page_text[i:i + chunk_page_size])
+
+        return self.chunks
 
 def main():
-    pages = open_documents("test_data/spytm_Scanned.pdf")
-    text = extract_pages(pages)
-    save_text(text)
+    doc = DocumentProcessor("test_data/Questioned_documents.pdf")
 
-def open_documents(filepath):
-    return pymupdf.open(filepath)
-       
-def extract_pages(pages):
-    extracted_text = []
+    doc.open_documents()
+    doc.extract_pages()
+    doc.chunk_page()
 
-    for page in pages:
-        text = page.get_text()
+    print(f"Pages: {len(doc.page_text)}")
+    print(f"Chunks: {len(doc.chunks)}")
 
-        if not text.strip():
-            pix = page.get_pixmap()
-            image = Image.open(BytesIO(pix.tobytes("png")))
-
-            text = perform_ocr(image)
-
-        extracted_text.append(text)
-
-    return extracted_text
-
-def perform_ocr(image):
-    return pytesseract.image_to_string(image)
-
-def save_text(text):
-    with open("output/result.txt", "a") as file:
-        file.write("\n\n".join(text))
 
 if __name__ == "__main__":
     main()
